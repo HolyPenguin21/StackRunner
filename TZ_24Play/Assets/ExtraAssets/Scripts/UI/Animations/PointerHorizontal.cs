89d7ed919df98a6b96dfa1f95cc455e5f2df4b93
HolyPenguin21 <31472244+HolyPenguin21@users.noreply.github.com>
2022-07-21 19:50:34 +0300

    sliderAnimationCorrection

--- TZ_24Play/Assets/ExtraAssets/Scripts/UI/Animations/PointerHorizontal.cs
+++ TZ_24Play/Assets/ExtraAssets/Scripts/UI/Animations/PointerHorizontal.cs
@@ -3,9 +3,20 @@ using DG.Tweening;
 
 public class PointerHorizontal : MonoBehaviour
 {
+    [SerializeField] RectTransform sliderImage;
+    RectTransform rectTransform;
+
+    [SerializeField] private float offset = 20f;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
     private void OnEnable()
     {
-        transform.DOLocalMoveX(transform.position.x + 100, 1f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        float x_Pos = rectTransform.localPosition.x + sliderImage.rect.width - offset;
+        rectTransform.DOLocalMoveX(x_Pos, 1f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
     }
 
     private void OnDisable()
